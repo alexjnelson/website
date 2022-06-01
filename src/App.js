b047c901355c3f242acb3fddf297413af27f422a
alexjnelson <alexj.nelson9@gmail.com>
2022-06-01 08:46:29 -0400

    added scrolling on certain key presses for accessibilities

--- src/App.js
+++ src/App.js
@@ -4,23 +4,44 @@ import Card1 from "./Card1";
 import Card2 from "./Card2";
 
 function App() {
+  const scrollKeys = [
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "PageUp",
+    "PageDown",
+    " ",
+  ];
+
   const [scrolled, setScrolled] = useState(false);
   const [card1, setCard1] = useState(true);
-
 
   const handler = (e) => {
     setScrolled(true);
   };
 
+  const keyHandler = (e) => {
+    if (scrollKeys.some((k) => k === e.key)) {
+      setScrolled(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", keyHandler);
+    return () => {
+      window.removeEventListener("keydown", keyHandler);
+    };
+  }, []);
+
   useEffect(() => {
     // make sure the card is only swapped once every 300 ms (this is half of the animation time,
     // so spinning can be simulated on continuous scrolling)
-    if (scrolled){
+    if (scrolled) {
       setCard1(!card1);
       setTimeout(() => setScrolled(false), 300);
     }
-  }, [scrolled])
-
+  }, [scrolled]);
 
   return (
     <div className="App" onWheel={handler} onTouchMove={handler}>
