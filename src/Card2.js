643ba196767b0e1b16c3a9f172733d9f5913b0c5
alexjnelson <alexj.nelson9@gmail.com>
2022-06-02 10:26:28 -0400

    fixed breakpoints on medium-sized screens

--- src/Card2.js
+++ src/Card2.js
@@ -22,15 +22,15 @@ export default function Card2(props) {
         <Col xs={12}>
           <Row>
             <Col xs={9}>
-              <h4 className="d-none d-sm-block accent">
+              <h4 className="d-none d-lg-block accent">
                 Ivey Business School, Western University
               </h4>
-              <h4 className="d-sm-none accent">Western University</h4>
+              <h4 className="d-lg-none accent">Western University</h4>
             </Col>
             <Col xs={3}>
-              <h4 className="text-end d-none d-sm-block">2023</h4>
+              <h4 className="text-end d-none d-lg-block">2023</h4>
             </Col>
-            <Col className="pb-3 pb-sm-5" xs={12}>
+            <Col className="pb-3 pb-lg-5" xs={12}>
               <h5>Software Engineering and Business Administration</h5>
             </Col>
           </Row>
@@ -42,9 +42,9 @@ export default function Card2(props) {
               <h4 className="accent">MoveMate</h4>
             </Col>
             <Col xs={3}>
-              <h4 className="text-end d-none d-sm-block">2022</h4>
+              <h4 className="text-end d-none d-lg-block">2022</h4>
             </Col>
-            <Col className="pb-3 pb-sm-5" xs={12}>
+            <Col className="pb-3 pb-lg-5" xs={12}>
               <h5>Software Developer</h5>
             </Col>
           </Row>
@@ -56,9 +56,9 @@ export default function Card2(props) {
               <h4 className="accent">Capella</h4>
             </Col>
             <Col xs={3}>
-              <h4 className="text-end d-none d-sm-block">2021</h4>
+              <h4 className="text-end d-none d-lg-block">2021</h4>
             </Col>
-            <Col className="pb-3 pb-sm-5" xs={12}>
+            <Col className="pb-3 pb-lg-5" xs={12}>
               <h5>CTO & Cofounder</h5>
             </Col>
           </Row>
@@ -69,17 +69,17 @@ export default function Card2(props) {
 
   return (
     <Row className={thisClass}>
-      <Col xs={1} sm={3} />
+      <Col xs={1} lg={3} />
 
-      <Col xs={6} className='d-none d-sm-block'>
+      <Col xs={6} className='d-none d-lg-block'>
         {makeData('align-content-center', { height: "100vh" })}
       </Col>
 
-      <Col xs={10} className='d-block d-sm-none'>
+      <Col xs={10} className='d-block d-lg-none'>
         {makeData('align-content-center', { height: "60vh" })}
       </Col>
 
-      <Col xs={1} sm={3} />
+      <Col xs={1} lg={3} />
     </Row>
   );
 }
