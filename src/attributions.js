1b869802a5e88eeaba800301bcd5b3583db89b20
alexjnelson <alexj.nelson9@gmail.com>
2022-06-01 22:23:32 -0400

    removed old files

--- src/attributions.js
+++
@@ -1,7 +0,0 @@
-export default function Attributions (props) {
-    return (
-        <div>
-            <a href='https://www.freepik.com/vectors/dots'>Dots vector created by starline - www.freepik.com</a>
-        </div>
-    )
-}
